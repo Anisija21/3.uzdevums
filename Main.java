d590575cd15c8b399ed18cfd05912553e8f48830
Anisija <anisija8@inbox.lv>
2021-02-10 20:11:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+<!DOCTYP HTML>
+<html>
+<body>
+
+<h2>The = Operator</h2>
+
+<p id="demo"></p>
+
+<script>
+var garums = 5;
+var platums = 6;
+var augstums = 3;
+var skaits = 9;
+var izmers1 = 20;
+var izmers1 = 15;
+var skaits = 9;
+document.getElementById("demo").innerHTML = e;
+</script>
+
+</body>
+<html>
+
+
